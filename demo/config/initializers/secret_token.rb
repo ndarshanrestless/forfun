4d31f6ff7116c82406b7f5135b91c6563a4f880e
ndarshanrestless <darshan@restlessbandit.com>
2016-02-18 22:28:44 +0000

    My first Ruby helloworld app

---
+++ demo/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Demo::Application.config.secret_key_base = '2b71ee9c3fc0c32341b69dce176363d944b0ffc24e138d4ac2730a02f99dafac78b4a7f1dd46e5f9cfef307d01d76c07b1853fa71e4df2248666634793b2130e'
